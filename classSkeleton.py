3d6b0fd83da82218a409634574702f46e0c09309
icummings1325 <isabella.cummigs001@albright.edu>
2026-03-23 12:27:02 -0400

    radar and ticket class

---
+++ classSkeleton.py
@@ -0,0 +1,27 @@
+class Radar:
+    def __init__(self, speed, liscence_plate, number_of_cars, ticket_quota):
+        self.speed = speed
+        self.liscence_plate = liscence_plate
+        self.number_of_cars = number_of_cars
+        self.ticket_quota = ticket_quota
+
+    def check_speed(self):
+
+    def check_liscence_plate(self):
+
+    def check_number_of_cars(self):
+
+    def carsPerMinute(self):
+
+    def daily_ticket_quota(self):
+
+
+class Ticket(self, liscence_plate, speed, fine_amount):
+    def __init__(self, liscence_plate, speed, fine_amount):
+        self.liscence_plate = liscence_plate
+        self.speed = speed
+        self.fine_amount = fine_amount
+
+    def calculate_fine(self):
+
+    def issue_ticket(self):
